28d6d045d46897c9452d15fc9f550d2f332f71d3
Wajid Javed <wajidjaved17@gmail.com>
2019-07-17 19:14:35 +0500

    csv report funcationaltiy

---
+++ src/controllers/uploadCSV/upload-loan-csv.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const randomString = require("randomstring");
+const responses = require('../../utilities/responses');
+const statusCodes = require('http-status-codes');
+const responseMsg = require("../../../constants/response-messages");
+
+const emailUtils = require('../../utilities/emails').employeeAddedEmail;
+const hashPasswordUtility = require('../../utilities/password').hashPassword;
+const model = require('../../models');
+const Op = model.Sequelize.Op;
+const equipmentsModel = 'equipments';
+
+const fs = require('fs'); 
+const csv = require('csv-parser');
+
+
+/**
+ * Post new Equipment
+ * @param req is containing payload sent from user
+ * @param callback will return response to handler
+ */
+module.exports = (req, callback) => {
+
+    let reqArray=[]
+  let inputFilePath='C:/xampp/htdocs/upload/test.csv'
+    fs.createReadStream(inputFilePath)
+    .pipe(csv())
+    .on('data', function(data){
+        try {
+            //perform the operation
+            console.log(data);
+            let dateobj={
+                equipmentName:data.equipmentName,
+                device1:data.device1
+            }
+           reqArray.push(dateobj);
+        }
+        catch(err) {
+            //error handler
+        }
+    })
+    .on('end',function(){
+        //some final operation
+
+        model[equipmentsModel].bulkCreate(reqArray).then(created => {
+              
+            return callback(null, responses.dataResponse(statusCodes.OK, responseMsg.ADDITION_SUCCESSFULL, created));
+    
+    
+        }).catch(err => {
+            return callback(err)
+        });
+
+    });  
+   
+
+
+
+          
+
+     
+}
--- src/routes/uploadcsvroute.js
+++ src/routes/uploadcsvroute.js
@@ -7,20 +7,49 @@ const responses = require('../utilities/responses');
 const config = require('../../configs/config');
 
 const validator = require('../validators/equipment');
-const uploadCSV = require('../controllers/uploadCSV/upload-csv');
+const uploadloanCSV = require('../controllers/uploadCSV/upload-loan-csv');
+const uploadlistCSV = require('../controllers/uploadCSV/upload-list-csv');
+
 
 
 module.exports = [
     {
         method: 'POST',
-        path: config.apiPrefix + '/upload/uploadCSV',
+        path: config.apiPrefix + '/upload/uploadLoanCSV',
         config: {
             description: 'uploadCSV',
             notes: 'uploadCSV.',
             tags: ['api', 'Equipment'],
             auth: false,
             handler: (request, reply) => {
-                uploadCSV(request.payload, (err, results) => {
+                uploadloanCSV(request.payload, (err, results) => {
+                    if (err) {
+                        console.log(err);
+                        reply(responses.makeMessageResponse(false, statusCodes.EXPECTATION_FAILED, err.message.replace(/[^a-zA-Z ]/g, ''))).code(statusCodes.INTERNAL_SERVER_ERROR);
+                    } else {
+                        reply(results);
+                    }
+                });
+            },
+            validate: {
+                payload: validator.uploadCSV,
+                failAction: (request, reply, source, err) => {
+                    reply(responses.makeMessageResponse(false, statusCodes.BAD_REQUEST, err.message.replace(/[^a-zA-Z ]/g, '')));
+                }
+            },
+            plugins: plugins.swaggerPlugin
+        }
+    },
+    {
+        method: 'POST',
+        path: config.apiPrefix + '/upload/uploadListCSV',
+        config: {
+            description: 'uploadCSV',
+            notes: 'uploadCSV.',
+            tags: ['api', 'Equipment'],
+            auth: false,
+            handler: (request, reply) => {
+                uploadlistCSV(request.payload, (err, results) => {
                     if (err) {
                         console.log(err);
                         reply(responses.makeMessageResponse(false, statusCodes.EXPECTATION_FAILED, err.message.replace(/[^a-zA-Z ]/g, ''))).code(statusCodes.INTERNAL_SERVER_ERROR);
